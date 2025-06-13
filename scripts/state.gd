2aba615f0695ad4bf6d0f55c25ea7cbb28a2530a
Poggs5401 <n00212320@iadt.ie>
2025-06-13 18:08:21 +0100

    Dialog Options Expanded + Global Reputation
    Expanded the responses an NPC can give.
    
    Added an additional merchant NPC.
    
    Added a global reputation variable that tracks reputation between NPC conversations.
    
    Added the framework for an inventory system.
    
    Added the framework for a currency system.

--- scripts/state.gd
+++ scripts/state.gd
@@ -1,4 +1,6 @@
 extends Node
 
-var nathan_reputation: int = 15
+var nathan_reputation: int = 5
 var has_rare_artifact: bool = false
+var player_money: int = 100 # Initial money for the player (adjusted from 10, common starting point)
+var rare_artifact_cost: int = 100 # Cost of the artifact from the merchant
